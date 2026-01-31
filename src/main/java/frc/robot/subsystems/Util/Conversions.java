4beaa8bb9991d05d8d23d291e031ecc5fb272d93
JakobTheDuck <9962082@bedfordnhk12.net>
2026-01-31 14:56:22 +0000

    added conversions class

--- src/main/java/frc/robot/subsystems/Util/Conversions.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.Util;
-
-public class Conversions {
-    
-    public static double falconToDegrees(double positionCounts, double gearRatio) {
-		return positionCounts * (360.0 / gearRatio);
-}
