883c2ce11c1a9d52c124dcc33a1263e729ae844f
Zac Engracia <9964262@bedfordnhk12.net>
2026-02-05 21:21:14 +0000

    redone completely ledlights. still need to finalize boolean suppliers and figure out the firing blinking led ligtht state

--- src/main/java/frc/robot/subsystems/LEDLights.java
+++ src/main/java/frc/robot/subsystems/LEDLights.java
@@ -1,113 +1,71 @@
 package frc.robot.subsystems;
 
+import java.util.Map;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
 
 public class LEDLights {
-    private final AddressableLED kLED;
-    private final AddressableLEDBuffer kBuffer;
+    private final AddressableLED kAddressableLED;
+    private final AddressableLEDBuffer kLEDBuffer;
     private final <Optional> alliance;
 
+    private final Map<Double, Color> maskSteps;
+    private final LEDPattern mask;
+    
 
-    public LEDLights(BooleanSupplier ){
+
+
+
+
+    public LEDLights(
+        BooleanSupplier climbing, 
+        BooleanSupplier cantAim
+    ){
+
         kLED = new AddressableLED(0);
         kBuffer = new AddressableLEDBuffer(0);
         alliance = DriverStation.getAlliance();
         KlED.setLength(kLength);
 
+        Map<Double, Color> maskSteps = Map.of(0, Color.kWhite, 0.5, Color.kBlack);
+        LEDPattern mask =
+            LEDPattern.steps(maskSteps).scrollAtRelativeSpeed(Percent.per(Second).of(0.10));
 
-        public enum TurretStates{
-            NONE(LEDPattern.solid(alliance)),
-            HOPPER(LEDPattern.solid(Color.kOrange)),
-            NEUTRALZONE_FEED_LEFT(LEDPattern.solid(Color.k)),
-            NEUTRALZANE_FEED_RIGHT(LEDPattern.solid(Color.k)),
-            OPPOSING_ALLIANCE_FEED_LEFT(LEDPattern.solid(Color.k)),
-            OPPOSING_ALLIANCE_FEED_RIGHT(LEDPattern.solid(Color.k));
+        public enum UnderGlow {
+            PASSIVE(LEDPattern.solid(alliance)),
+            AUTO_COMPLETED(LEDPattern.solid(Color.kGreen));
+            CLIMB(LEDPattern.rainbow(255, 188));
+        
+            private final LEDPattern underGlowState;
 
+            public UnderGlow(){
+                this.underGlowState = underGlowState;
+            }
+
+            public void applyTo(AddressableLEDBuffer kLEDBuffer){
+                underGlowState.applyTo(kLEDBuffer);
+            }
+        }
+
+        public enum Turret {
+            PASSIVE(alliance.breathe(Seconds.of(2))),
+            // FIRING(find out),
+            CANT_AIM(alliance.mask(mask)),
+            AUTO_COMPLETED(LEDPattern.solid(Color.kGreen)),
+            CLIMB(LEDPattern.rainbow(255, 188));
+            
             private final LEDPattern turretState;
-
-            public TurretStates(LEDPattern turretPattern){
-                this.turretPattern = turretPattern;
-        }
-
-        public void applyTo(AddressableLEDBuffer kBuffer){
-            turretPattern.applyTo(kBuffer);
-    }
-
-        public enum HopperStates{
-            PASSIVE(),
-            INDEXING(),
-            INTAKING(),
-            INTAKE_AND_INDEXING(),
-            FEED(),
-            OUTTAKE();
-
-            private final LEDPattern hopperPattern;
-
-            public HopperState(LEDPattern hopperPattern){
-                this.hopperPattern = hopperPattern;
+            
+            public Turret(){
+                this.turretState = turretState;
             }
 
-            public void applyTo(AddressableLEDBuffer kBuffer){
-                hopperPattern.applyTo(kBuffer);
+            public void applyTo(AddressableLEDBuffer kLEDBuffer){
+                turretState.applyTo(kLEDBuffer);
             }
         }
-
-        public enum IndexerStates{
-            PASSIVE(),
-            LEFT(),
-            RIGHT(),
-            LEFT_AND_RIGHT();
-
-            private final LEDPattern indexerPattern;
-
-            public IndexerStates(LEDPattern indexerPattern){
-                this.indexerPattern = indexerPattern;
-            }
-
-            public void applyTo(AddressableLEDBuffer kBuffer){
-                indexerPattern.applyTo(kBuffer);
-        }
-    
-}
-    
-        public void displayLED(){
-            switch(turretState){
-                case (NONE):
-                    turretPattern = TurretStates.NONE;
-                case (HOPPER):
-                    turretPattern = TurretStates.HOPPER;
-                case (NEUTRAL_ZONE_FEED_LEFT):
-                    turretPattern = TurretStates.NEUTRALZANE_FEED_LEFT;
-                case (NEUTRAL_ZONE_FEED_RIGHT):
-                    turretPattern = TurretStates.NEUTRALZANE_FEED_RIGHT;
-            }
-
-            switch(hopperState){
-                case (PASSIVE):
-                    hopperPattern = HopperStates.PASSIVE;
-                case (INDEXING):
-                    hopperPattern = HopperStates.INDEXING;
-                case (INTAKING_AND_INDEXING):
-                    hopperPattern = HopperStates.INTAKEING_AND_INDEXING;
-                case (FEED):
-                    hopperPattern = HopperStates.FEED;
-                case (OUTTAKE):
-                    hopperPattern = HopperStates.OUTTAKE;
-            }
-
-            switch(indexerState){
-                case(PASSIVE):
-                    indexerPattern = IndexerStates.PASSIVE;
-                case(LEFT):
-                    indexerPattern = IndexerStates.LEFT;
-                case(RIGHT):
-                    indexerPattern = IndexerStates.RIGHT;
-                case(LEFT_AND_RIGHT):
-                    indexerPattern = IndexerStates.LEFT_AND_RIGHT;
-            }
-
             public void setState(){
                 turretPattern.applyTo(kBuffer);
                 hopperPattern.applyTo(kBuffer);
