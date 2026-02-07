fec2c2508deb06c6792bee49523ee282cf526d7a
redstorm509 <redstorm509@gmail.com>
2026-02-06 20:18:24 -0500

    Made LEDLights class and started working on enums for states

--- src/main/java/frc/robot/subsystems/LEDLights.java
+++ src/main/java/frc/robot/subsystems/LEDLights.java
@@ -4,106 +4,39 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
 
-public class LEDLights {
-    private final AddressableLED kLED;
-    private final AddressableLEDBuffer kBuffer;
+public class LEDLights extends SubsystemBase {
+    AddressableLED m_led = new AddressableLED(0); //replace ID with constant later
 
+    AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(123);
+  
+    public enum underglowStates {
+        PASSIVE(),
+        AUTO_COMPLETE(),
+        CLIMB();
+    }
+    
+    public enum turretStates {
+        PASSIVE(LEDPattern.solid(kRed).breathe(Seconds.of(2)), () -> 100), //Replace kRed with constant for alliance color later
+        FIRING(LEDPattern.solid(kRed), tempConstants.shooterIntensity),  //Replace kRed with constant for alliance color later
+        CANT_AIM(LEDPattern.steps(Map.of(0, Color.kWhite, 0.5, Color.kBlack)).scrollAtRelativeSpeed(Percent.per(Second).of(0.25)), ),
+        AUTO_COMPLETED(),
+        CLIMBING();
 
-    public LEDLights(){
-        kLED = new AddressableLED(0);
-        kBuffer = new AddressableLEDBuffer(0);
-        KlED.setLength(kLength);
+        LEDPattern pattern;
+        DoubleSupplier intensity;
 
+        private turretStates(LEDPattern pattern, DoubleSupplier intensity) {
+            this.pattern = pattern;
+            this.intensity = intensity;
 
-        public enum TurretStates{
-            NONE(),
-            HOPPER(),
-            NEUTRALZONE_FEED_LEFT(),
-            NEUTRALZANE_FEED_RIGHT(),
-            OPPOSING_ALLIANCE_FEED_LEFT(),
-            OPPOSING_ALLIANCE_FEED_RIGHT();
-
-            private final LEDPattern turretState;
-
-            public TurretStates(LEDPattern turretPattern){
-                this.turretPattern = turretPattern;
         }
-
-        public void applyTo(AddressableLEDBuffer kBuffer){
-            turretPattern.applyTo(kBuffer);
     }
 
-        public enum HopperStates{
-            PASSIVE(),
-            INDEXING(),
-            INTAKING(),
-            INTAKE_AND_INDEXING(),
-            FEED(),
-            OUTTAKE();
-
-            private final LEDPattern hopperPattern;
-
-            public HopperState(LEDPattern hopperPattern){
-                this.hopperPattern = hopperPattern;
-            }
-
-            public void applyTo(AddressableLEDBuffer kBuffer){
-                hopperPattern.applyTo(kBuffer);
-            }
-        }
-
-        public enum IndexerStates{
-            PASSIVE(),
-            LEFT(),
-            RIGHT(),
-            LEFT_AND_RIGHT();
-
-            private final LEDPattern indexerPattern;
-
-            public IndexerStates(LEDPattern indexerPattern){
-                this.indexerPattern = indexerPattern;
-            }
-
-            public void applyTo(AddressableLEDBuffer kBuffer){
-                indexerPattern.applyTo(kBuffer);
-        }
+    public LEDLights(){
+        m_led.setLength(m_ledBuffer.getLength());
+        m_led.setData(m_ledBuffer);
+        m_led.start();
     
-}
-    
-        public void displayLED(){
-            switch(turretState){
-                case (NONE):
-                    turretPattern = TurretStates.NONE;
-                case (HOPPER):
-                    turretPattern = TurretStates.HOPPER;
-                case (NEUTRAL_ZONE_FEED_LEFT):
-                    turretPattern = TurretStates.NEUTRALZANE_FEED_LEFT;
-                case (NEUTRAL_ZONE_FEED_RIGHT):
-                    turretPattern = TurretStates.NEUTRALZANE_FEED_RIGHT;
-            }
 
-            switch(hopperState){
-                case (PASSIVE):
-
-                case (INDEXING):
-
-                case (INTAKING_AND_INDEXING):
-
-                case (FEED):
-
-                case ()
-            }
-
-            switch(indexerState){
-                case
-            }
-
-            public void setState(){
-                turretPattern.applyTo(kBuffer);
-                hopperPattern.applyTo(kBuffer);
-                indexerPattern.applyTo(kBuffer);
-                ledLight.setData(kBuffer);
-            }
-        }
-
+    }
 }
