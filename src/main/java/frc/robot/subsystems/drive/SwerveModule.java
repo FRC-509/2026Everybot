1b72bc581ae9c985d28c8bbc9576fa81547f2257
Aaron_J_Blodgett <aaron.blodgett.ab@gmail.com>
2026-02-07 19:48:22 +0000

    Final error-added import for talon fx and canencoder

--- src/main/java/frc/robot/subsystems/drive/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveModule.java
@@ -7,6 +7,8 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.hardware.CANcoder;
 
 public class SwerveModule {
     public int moduleNumber;
